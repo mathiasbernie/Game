c8719f084e8b1c6f52c796c4d307b440eb12ad6a
mathiasbernie <mathias.berneland@gmail.com>
2015-11-11 14:40:35 +0100

    Added player

---
+++ app/src/main/java/com/mathias/game/GameObject.java
@@ -0,0 +1,59 @@
+package com.mathias.game;
+
+import android.graphics.Rect;
+
+/**
+ * Created by Mathias on 2015-11-11.
+ */
+public abstract class GameObject {
+
+    protected int x;
+    protected int y;
+    protected int dy;
+    protected int dx;
+    protected int width;
+    protected int height;
+
+    public void setX(int x) {
+
+        this.x = x;
+
+    }
+
+    public void setY(int y) {
+
+        this.y = y;
+
+    }
+
+    public int getX() {
+
+        return x;
+
+    }
+
+    public int getY() {
+
+        return y;
+
+    }
+
+    public int getHeight() {
+
+        return height;
+
+    }
+
+    public int getWidth() {
+
+        return width;
+
+    }
+
+    public Rect getRectangle() {
+
+        return new Rect(x, y,x+width, y+height);
+
+    }
+
+}
